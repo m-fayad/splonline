576e53cb96821c0ee040aa0bd7e309032fe97520
muhammed-fayad <mohammed.yuossry@gmail.com>
2026-02-17 22:10:53 +0200

    navbar & footer

--- src/components/VSC-client/Navbar/Navbar.tsx
+++ src/components/VSC-client/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { Button } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
@@ -23,26 +24,70 @@ const Navbar = () => {
   // });
 
   return (
-    <header className="border-b-2 border-b-[#136e82] bg-[#153c3f] h-10 flex items-center">
-      {links.map((link) => {
-        const isActive = pathname === link.href;
-        return (
-          <Link
-            key={link.href}
-            to={link.href}
-            className={cn(
-              `px-4 h-full flex items-center transition-colors text-sm`,
-              {
-                "text-white bg-[#146e82]": isActive,
-                "text-white/50 hover:text-white hover:bg-[#146e82]/50":
-                  !isActive,
+    <header>
+      <nav className="border-b-4 border-b-[#136e82] bg-[#153c3f]">
+        <div className="md:container flex items-center h-10 md:h-8">
+          {links.map((link) => {
+            console.log(pathname);
+            const isActive = pathname === link.href;
+            return (
+              <Link
+                key={link.href}
+                to={link.href}
+                className={cn(
+                  `px-4 md:px-6 h-full flex items-center transition-colors text-sm`,
+                  {
+                    "text-white bg-[#146e82] font-semibold": isActive,
+                    "text-white/50 hover:text-white hover:bg-[#146e82]/50":
+                      !isActive,
+                  },
+                )}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
+      </nav>
+
+      <div className="flex justify-between gap-4 items-center">
+        <img src="/assets/images/new/logo.svg" alt="logo" />
+
+        <div className="flex items-center">
+          <Button
+            variant="text"
+            sx={{
+              color: "#146e82",
+              fontWeight: "bold",
+              border: { md: "1px solid #146e82" },
+              "&:hover": {
+                border: { md: "1px solid #146e82" },
+                backgroundColor: "rgba(20, 110, 130, 0.04)",
               },
-            )}
+            }}
           >
-            {link.name}
-          </Link>
-        );
-      })}
+            دخول
+          </Button>
+
+          <div
+            className="md:hidden h-6 w-[1px] bg-[#146e82]/20"
+            aria-hidden="true"
+          />
+
+          <Button
+            variant="text"
+            sx={{
+              color: "#146e82",
+              fontWeight: "bold",
+              "&:hover": {
+                backgroundColor: "rgba(20, 110, 130, 0.04)",
+              },
+            }}
+          >
+            تسجيل
+          </Button>
+        </div>
+      </div>
     </header>
   );
 };
